fe49e40741f8251e2fc968310f874ef29c7a5fa0
sizan378 <sizanmahmud08@gmail.com>
2024-01-30 17:10:52 +0600

    ✨ feat(toolbar | swagger): toolbar | swagger
    setup django debug toolbar and swagger

--- newspaper/settings/dev_settings.py
+++ newspaper/settings/dev_settings.py
@@ -1,1 +1,27 @@
 from .settings import *
+
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+    'drf_yasg',
+]
+
+
+MIDDLEWARE += [
+   'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [                                              
+    '127.0.0.1',                                              
+]                                                             
+
+def show_toolbar(request):                                    
+    return True                                                
+
+DEBUG_TOOLBAR_CONFIG = {                                      
+    "SHOW_TOOLBAR_CALLBACK" : show_toolbar,                   
+}                                                             
+
+if DEBUG:                                                     
+    import mimetypes                                                    
+    mimetypes.add_type("application/javascript", ".js", True) 

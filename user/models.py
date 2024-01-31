c374362508e5e126cd6f6835b2ad8e2c4e6e8a28
sizan378 <sizanmahmud08@gmail.com>
2024-01-31 11:41:16 +0600

    ✨ feat(custom user model): custom user model
    create custom user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=100, null=True, blank=True)
+    last_name = models.CharField(_("last name"), max_length=100, null=True, blank=True)
+    phone_number = models.CharField(_("phone number"), max_length=14, null=True, blank=True)
+    address = models.CharField(_("address"), max_length=255, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
